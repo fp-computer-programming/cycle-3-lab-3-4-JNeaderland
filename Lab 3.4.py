b062064f9aa647a0b07d2e033276087911b7d5bc
JNeaderland <144148081+JNeaderland@users.noreply.github.com>
2023-09-28 12:07:48 -0400

    Add files via upload

---
+++ Lab 3.4.py
@@ -0,0 +1,42 @@
+import math
+
+a = math.ceil(7.15)
+print (a)
+#Me: 8
+#Python: 8
+a = math.floor(19.156)
+print (a)
+#Me: 19
+#Python: 19
+a = math.fabs(-42)
+print (a)
+#Me: 42
+#Python: 42.0
+a = math.pow(2, 3)
+print (a)
+#Me: 8
+#Python: 8.0
+a = math.log10(100)
+print (a)
+#Me: 2
+#Python: 2.0
+a = math.sqrt(9)
+print (a)
+#Me: 3
+#Python: 3.0
+a = math.isqrt(2)
+print (a)
+#Me: 1
+#Python: 1
+a = math.factorial(5)
+print (a)
+#Me: 120
+#Python: 120
+a = math.gcd(111111111, 12321)
+print (a)
+#Me: 333
+#Python 333
+a = math.sin(0)
+print (a)
+#Me: 0
+#Python: 0.0
